b6626fc352969f18c092702c41c398a39c069609
John Angel <j_angeldiaz@hotmail.com>
2019-06-02 08:49:11 -0500

    Feature: Project Dashboard - Change project action icon based on current status (Play/Stop)

--- src/project/projectComponent.js
+++ src/project/projectComponent.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
 
 class Project extends Component{
+
+    state = {
+        icon: faPlay
+    }
+    changeState = () => this.state.icon === faPlay ? this.setState({icon:faStop}) : this.setState({icon:faPlay})
+    
     render(){
         return(
             <div className={'project'}>
-                <FontAwesomeIcon className={'icon'} icon={faPlay} />
+                <FontAwesomeIcon className={'icon'} icon={this.state.icon} onClick={this.changeState} />
             </div>
 
         )        
