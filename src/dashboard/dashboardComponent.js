bee525e86902381a569052598bead1b661b3a778
John Angel <j_angeldiaz@hotmail.com>
2019-08-22 07:50:00 -0500

    Projects test data moved out of Dashboard component

--- src/dashboard/dashboardComponent.js
+++ src/dashboard/dashboardComponent.js
@@ -1,102 +1,11 @@
 import React, {Component} from 'react';
 import DashboardItemContainer from './dashboardItemContainer'
 import Project from '../project/projectComponent';
+import {projects} from '../project/testData';
 
 class Dashboard extends Component{
 
-    projects = [
-        {
-          id: 1,
-          name: 'Rocket',
-          description: 'Rocket to Mars',
-          start: '2018-01-05',
-          plannedEnd: '2019-05-30',
-          actualEnd: '2019-05-15',
-          status:'Complete'
-        },
-        {
-          id: 2,
-          name: 'House',
-          description: 'House in the country side',
-          start: '2018-04-12',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'Risk'
-        },
-        {
-          id: 3,
-          name: 'Book',
-          description: 'Book for children',
-          start: '2018-09-20',
-          plannedEnd: '2019-04-30',
-          actualEnd: '',
-          status:'NotStarted'
-        }
-        ,
-        {
-          id: 4,
-          name: 'Job',
-          description: 'Front End developer job',
-          start: '2019-08-01',
-          plannedEnd: '2019-11-30',
-          actualEnd: '',
-          status:'OnTrack'
-        },
-        {
-          id: 5,
-          name: 'Drawing',
-          description: 'Improve drawing skills',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'Delayed'
-        },
-        {
-          id: 6,
-          name: 'Swimming',
-          description: 'Improve swimming skills',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'Complete'
-        },
-        {
-          id: 7,
-          name: 'Dancing',
-          description: 'Improve dancing skills',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'Risk'
-        },
-        {
-          id: 8,
-          name: 'Car',
-          description: 'Buy a car',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'NotStarted'
-        },
-        {
-          id: 9,
-          name: 'Cooking',
-          description: 'Improve cooking skills',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'OnTrack'
-        },
-        {
-          id: 10,
-          name: 'Writing',
-          description: 'Improve writing skills',
-          start: '2019-06-01',
-          plannedEnd: '2019-12-30',
-          actualEnd: '',
-          status:'Delayed'
-        }
-    ]
+    
 
     state = {
       projectActive: 0
@@ -114,7 +23,7 @@ class Dashboard extends Component{
           </div>
             <div className={"projectsContainer"}>
             {
-                this.projects.map(project => (
+                projects.map(project => (
                   
                     
                       project.id === this.state.projectActive ?
