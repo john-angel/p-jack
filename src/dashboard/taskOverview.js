450e4de8abbc843fc46c0cc33cd6a90cd7cc77da
John Angel <j_angeldiaz@hotmail.com>
2019-09-20 05:31:27 -0500

    Feature: Project Dashboard. Attribute type set in tasks list and dummy total number of tasks KPI added

--- src/dashboard/taskOverview.js
+++ src/dashboard/taskOverview.js
@@ -4,8 +4,9 @@ class TaskOverview extends Component{
     render(){
         return (
             <ul className={'taskOverviewList'}>
-                <li><a href='a'>Current task that can be long</a></li>
-                <li><a href='b'>Next task</a></li>                
+                <li><a href='/tasks' type='text/html'>Current or due task that can be long</a></li>
+                <li><a href='/tasks' type='text/html'>Next task</a></li>
+                <li><a href='/tasks' type='text/html'>{15} to go</a></li>                
             </ul>
         )
     }
