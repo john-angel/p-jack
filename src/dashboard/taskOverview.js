ef5084733e5ef0fbfee5f94827ddd4293cf8fc3b
John Angel <j_angeldiaz@hotmail.com>
2019-09-21 07:52:28 -0500

    Feature: Project Dashboard. - Style update test on task overview list to differentiate by color the status of the task (On track, delayed, etc) - App title updated.

--- src/dashboard/taskOverview.js
+++ src/dashboard/taskOverview.js
@@ -4,9 +4,9 @@ class TaskOverview extends Component{
     render(){
         return (
             <ul className={'taskOverviewList'}>
-                <li><a href='/tasks' type='text/html'>Current or due task that can be long</a></li>
-                <li><a href='/tasks' type='text/html'>Next task</a></li>
-                <li><a href='/tasks' type='text/html'>{15} to go</a></li>                
+                <li><a href='/tasks' type='text/html' style={{color:'#FF8080'}}>Current or due task that can be long.</a></li>
+                <li><a href='/tasks' type='text/html' style={{color:'#7ED3B2'}}>Next task.</a></li>
+                <li><a href='/tasks' type='text/html' style={{fontSize:'2em', color:'black'}}>{15}</a> to go.</li>                
             </ul>
         )
     }
