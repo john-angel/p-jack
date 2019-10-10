31cfcdef2faf0a589b54d39975a568d2ac41cebc
John Angel <j_angeldiaz@hotmail.com>
2019-10-09 21:04:10 -0500

    Feature: Task Status Chart - Display the pie chart only if there are tasks created. - Display the option to add a task in case the project doesn't have any.

--- src/dashboard/taskStatusChart.js
+++ src/dashboard/taskStatusChart.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus} from '@fortawesome/free-solid-svg-icons'
 
 class TaskStatusChart extends Component{
 
@@ -15,75 +16,85 @@ class TaskStatusChart extends Component{
         dataTable.addColumn('string','Status');
         dataTable.addColumn('number','Amount');
 
-        this.props.tasks.forEach(task => {
+        if(this.props.tasks.length){
+            this.props.tasks.forEach(task => {
             
-            switch(task.status){
-                case 'notStarted': 
-                    tasks.notStarted++;
-                    break;
-                case 'onHold':
-                    tasks.onHold++;
-                    break;
-                case 'onTrack':
-                    tasks.onTrack++;
-                    break;
-                case 'delayed':
-                    tasks.delayed++;
-                    break;
-                case 'atRisk':
-                    tasks.atRisk++;
-                    break;
-                case 'complete':
-                    tasks.complete++;
-                    break;
-                default:
-                    break;
+                switch(task.status){
+                    case 'notStarted': 
+                        tasks.notStarted++;
+                        break;
+                    case 'onHold':
+                        tasks.onHold++;
+                        break;
+                    case 'onTrack':
+                        tasks.onTrack++;
+                        break;
+                    case 'delayed':
+                        tasks.delayed++;
+                        break;
+                    case 'atRisk':
+                        tasks.atRisk++;
+                        break;
+                    case 'complete':
+                        tasks.complete++;
+                        break;
+                    default:
+                        break;
+                }
+            });
+    
+            if(tasks.notStarted > 0){
+                dataTable.addRow(['Not started',tasks.notStarted]);
+                slices.push({color:'#EBEBEB'});
             }
-        });
+            if(tasks.onHold > 0){
+                dataTable.addRow(['On hold',tasks.onHold]);
+                slices.push({color:'#CFCFCF'});
+            }
+            if(tasks.onTrack > 0){
+                dataTable.addRow(['On track',tasks.onTrack]);
+                slices.push({color:'#7ED3B2'});
+            }
+            if(tasks.delayed > 0){
+                dataTable.addRow(['Delayed',tasks.delayed]);
+                slices.push({color:'#FF8080'});
+            }
+            if(tasks.atRisk > 0){
+                dataTable.addRow(['At risk',tasks.atRisk]);
+                slices.push({color:'#FFBA92'});
+            }
+            if(tasks.complete > 0){
+                dataTable.addRow(['Complete',tasks.complete]);
+                slices.push({color:'#8AC6D1'});
+            }
+            var options = {
+                pieHole: 0.4,
+                tooltip:{trigger:this.props.tasks.length ? 'focus':'none'},
+                legend: {alignment:'center',position:'right'},
+                pieSliceText: 'none',
+                slices:slices,
+                chartArea: {left:2,top:5,width:'80%'}
+            };
+    
+            var chart = new window.google.visualization.PieChart(document.getElementById(this.props.divId));
+            chart.draw(dataTable, options);
 
-        if(tasks.notStarted > 0){
-            dataTable.addRow(['Not started',tasks.notStarted]);
-            slices.push({color:'#EBEBEB'});
         }
-        if(tasks.onHold > 0){
-            dataTable.addRow(['On hold',tasks.onHold]);
-            slices.push({color:'#CFCFCF'});
-        }
-        if(tasks.onTrack > 0){
-            dataTable.addRow(['On track',tasks.onTrack]);
-            slices.push({color:'#7ED3B2'});
-        }
-        if(tasks.delayed > 0){
-            dataTable.addRow(['Delayed',tasks.delayed]);
-            slices.push({color:'#FF8080'});
-        }
-        if(tasks.atRisk > 0){
-            dataTable.addRow(['At risk',tasks.atRisk]);
-            slices.push({color:'#FFBA92'});
-        }
-        if(tasks.complete > 0){
-            dataTable.addRow(['Complete',tasks.complete]);
-            slices.push({color:'#8AC6D1'});
-        }
-        
-        var options = {
-            pieHole: 0.4,
-            legend: {alignment:'center',position:'right'},
-            pieSliceText: 'none',
-            slices:slices,
-            chartArea: {left:2,top:5,width:'80%'}
-        };
-
-        var chart = new window.google.visualization.PieChart(document.getElementById(this.props.divId));
-        chart.draw(dataTable, options);
-        
-       
     }
+    
+    onAdd = () => console.log('onAdd'); 
 
     render(){
         return (
             <React.Fragment>
-                <div id={this.props.divId} style={{width: '270px', height: '180px'}}></div>
+                <div id={this.props.divId} style={{display:'flex',alignItems:'center',justifyContent:'center',width: '270px', height: '180px'}}>
+                    {
+                        !this.props.tasks.length ?
+                            <FontAwesomeIcon icon={faPlus} onClick={this.onAdd}></FontAwesomeIcon> :
+                            null                       
+                    }
+                
+                </div>
             </React.Fragment>
         )
     }
