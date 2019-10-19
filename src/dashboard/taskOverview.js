53b0d6c9a0e57c4315d4adc3555bfbeaaf7e5cbe
John Angel <j_angeldiaz@hotmail.com>
2019-10-18 23:08:00 -0500

    Refactor: Tasks overview

--- src/dashboard/taskOverview.js
+++ src/dashboard/taskOverview.js
@@ -1,38 +1,63 @@
 import React, {Component} from 'react';
-import {completeStatus} from '../utils/status';
+import {notStartedStatus,onHoldStatus,onTrackStatus,delayedStatus,atRiskStatus} from '../utils/status';
+import {getColorFromStatus} from '../utils/colors';
 
 class TaskOverview extends Component{
 
     constructor(props){
         super(props);
         let tasksTobeCompleted = 0;
+        let nextTask = '';
+        let taskColor = '';
 
         if(typeof this.props.tasks !== 'undefined'){
             const tasksArray = Object.keys(this.props.tasks).map(projectId => this.props.tasks[projectId]);
             
             tasksArray.forEach(task => {
-                if(task.status !== completeStatus){
-                    tasksTobeCompleted++;
+
+                switch (task.status) {
+                    case notStartedStatus:
+                    case onHoldStatus:
+                        tasksTobeCompleted++;
+                        break;                   
+                    case onTrackStatus:
+                    case atRiskStatus:
+                        if(nextTask === ''){ 
+                            nextTask = task.name;
+                            taskColor = getColorFromStatus(task.status);
+                        };                        
+                        tasksTobeCompleted++;
+                        break;
+                    case delayedStatus://Delayed tasks have a higher priority
+                        nextTask = task.name;
+                        taskColor = getColorFromStatus(task.status) ;
+                        tasksTobeCompleted++;
+                        break;                                   
+                    default:
+                        break;
                 }
+
+                
             })
-            this.state = {tasksToComplete: tasksTobeCompleted}
+            this.state = tasksTobeCompleted === 0 ? 
+                {task:'You are done!', color:getColorFromStatus(onTrackStatus), tasksToComplete:tasksTobeCompleted} :
+                {task:nextTask, color:taskColor, tasksToComplete:tasksTobeCompleted};
         }else{
-            this.state = {tasksToComplete: 0}
+            this.state = {task:'No tasks defined', color:getColorFromStatus(atRiskStatus)}
         }
 
     }
+    
     render(){
         return (
             <ul className={'taskOverviewList'}>
-                <li><a href='/tasks' type='text/html' style={{color:'#FF8080'}}>Current or due task that can be long.</a></li>
-                <li><a href='/tasks' type='text/html' style={{color:'#7ED3B2'}}>Next task.</a></li>
+
+                <li><a href='/tasks' type='text/html' style={{ fontSize: '1em', fontWeight:'bold', color: this.state.color }}>{this.state.task}</a></li>
                 {
-                    this.state.tasksToComplete === 0 ?
-                        <li>You're done!</li> :
-                        <li>
-                            <a href='/tasks' type='text/html' style={{ fontSize: '1.5em', color: 'black' }}>{this.state.tasksToComplete}</a> tasks to go
-                        </li>
-                }                          
+                    this.state.tasksToComplete > 0 ?
+                        <li><a href='/tasks' type='text/html' style={{ fontSize: '1.5em', color: 'black' }}>{this.state.tasksToComplete}</a> tasks to go</li> :
+                        null
+                }
             </ul>
         )
     }
