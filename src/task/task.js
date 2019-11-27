8dd783115c388eac3ec8feb1b523c0f3e366d21d
John Angel <j_angeldiaz@hotmail.com>
2019-11-27 17:24:21 -0500

    Feature: Task description
    - Add check icon to mark the task as complete.

--- src/task/task.js
+++ src/task/task.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 
 class Task extends Component {
 
     constructor(props){
         super(props);
-        this.state = {name: this.props.data.name};
+
+        this.state = {
+            name: this.props.data.name,
+            icon: faCircle
+        };
+    }
+
+    onCheckIconClick = (event) => {
+        console.log('onCheckIconClick');
+        this.setState({icon:this.state.icon === faCircle ? faCheckCircle : faCircle})
     }
 
     handleClick = (event) => {
@@ -18,9 +30,11 @@ class Task extends Component {
 
     render(){
         return (
-            <React.Fragment>                
-                <textarea className={'taskItem'} maxLength={'100'} rows={'2'} value={this.state.name} onClick={this.handleClick} onChange={this.handleChange}></textarea>                
-            </React.Fragment>
+            <div className={'taskItem'}>
+                <FontAwesomeIcon className={'taskCheckIcon'} icon={this.state.icon} onClick={this.onCheckIconClick}></FontAwesomeIcon>
+                <textarea className={'taskDescription'} maxLength={'100'} rows={'2'} value={this.state.name} onClick={this.handleClick} onChange={this.handleChange}></textarea>                
+            </div>
+                           
         )
     }
 }
