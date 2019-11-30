4f1fda2c131c48f81c0aa829317393520e2d4253
John Angel <j_angeldiaz@hotmail.com>
2019-11-30 08:21:40 -0500

    Feature: Task detail
    - Styles update.
    - Close detail icon changed.

--- src/task/taskDetail.js
+++ src/task/taskDetail.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandPointRight} from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import {getTextFromStatus} from '../utils/status';
 
 class TaskDetail extends Component{
@@ -11,11 +11,10 @@ class TaskDetail extends Component{
         return(
             <div className={'taskDetail'}>
                 <p>{this.props.data.name}</p>
-                <p>{this.props.data.plannedEnd}</p>
-                <p>{this.props.data.actualEnd}</p>
+                <p>{this.props.data.due}</p>
                 <p>{getTextFromStatus(this.props.data.status)}</p>
                 <p>{this.props.data.comments}</p>
-                <FontAwesomeIcon icon={faHandPointRight} style={{fontSize:'1em'}} onClick={this.onClose}></FontAwesomeIcon>
+                <FontAwesomeIcon id={'closeDetailIcon'} icon={faChevronRight} style={{fontSize:'1em'}} onClick={this.onClose}></FontAwesomeIcon>
             </div>
         )
     }
