c89f96f50d06d11b7e67f7ce2cf71420bb1ac4c4
John Angel <j_angeldiaz@hotmail.com>
2019-11-30 09:23:11 -0500

    Feature: Task detail
    - Textarea used for description.
    - Column added for icons on description and due date.
    - Group task item (Detail and Due date) on taskDetailItem div.

--- src/task/taskDetail.js
+++ src/task/taskDetail.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faCheckCircle,faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import {getTextFromStatus} from '../utils/status';
 
 class TaskDetail extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {  
+            descriptionTextDecoration: 'none',          
+            icon: faCircle
+        };
+    }
+
+    onCheckIconClick = () => {        
+        this.setState({
+            icon: this.state.icon === faCircle ? faCheckCircle : faCircle,
+            descriptionTextDecoration: this.state.descriptionTextDecoration === 'none' ? 'line-through' : 'none'
+        })
+    }
     
     onClose = () => this.props.onClose()
 
     render(){
         return(
             <div className={'taskDetail'}>
-                <p>{this.props.data.name}</p>
-                <p>{this.props.data.due}</p>
+                <div className={'taskDetailItem'}>
+                    <FontAwesomeIcon className={'taskDetailIcon'} icon={this.state.icon} onClick={this.onCheckIconClick}></FontAwesomeIcon>
+                    <textarea style={{textDecoration:this.state.descriptionTextDecoration}}>{this.props.data.name}</textarea>
+                </div>
+                <div className={'taskDetailItem'}>
+                    <FontAwesomeIcon className={'taskDetailIcon'} icon={faCalendarAlt}></FontAwesomeIcon>
+                    <p>{this.props.data.due}</p>
+                </div>
                 <p>{getTextFromStatus(this.props.data.status)}</p>
                 <p>{this.props.data.comments}</p>
                 <FontAwesomeIcon id={'closeDetailIcon'} icon={faChevronRight} style={{fontSize:'1em'}} onClick={this.onClose}></FontAwesomeIcon>
