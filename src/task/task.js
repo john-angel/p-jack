b0b54074b235b1f2625fdd16f98f7a4ff9abea27
John Angel <j_angeldiaz@hotmail.com>
2019-12-16 19:27:10 -0500

    Feature: Add task to an empty task list
    - Enable task name update.
    - Change icon when a task name is updated.

--- src/task/task.js
+++ src/task/task.js
@@ -6,29 +6,50 @@ import {completeStatus} from '../utils/status';
 
 class Task extends Component {
 
-    onCheckIconClick = () => {  
-        this.props.onTaskMarked(this.props.data.id);        
+    constructor(props){
+        super(props);
+        this.state = {
+            checkIcon: faPlus,
+            value: '', 
+    }
+    }
+
+    onCheckIconClick = () => {
+        if(this.props.data !== null){
+            this.props.onTaskMarked(this.props.data.id); 
+        }            
     }
 
     onNameClick = () => {
-        this.props.onSelected(this.props.data);
+        if(this.props.data !== null){
+            this.props.onSelected(this.props.data);
+        }
     }
       
     onNameChange = (event) => {
         event.persist();
-        this.props.onNameChange(this.props.data.id,event.target.value);
+        
+        if(this.props.data !== null){
+            this.props.onNameChange(this.props.data.id,event.target.value);
+        }else{
+            this.setState({
+                checkIcon:faCircle, 
+                value:event.target.value 
+            });
+        }
     }
 
     render(){
         
-        const checkIcon = this.props.data !== null ? this.props.data.status === completeStatus ? faCheckCircle : faCircle : faPlus;
+        const checkIcon = this.props.data !== null ? this.props.data.status === completeStatus ? faCheckCircle : faCircle : this.state.checkIcon;
         const descriptionDecoration = this.props.data !== null ? this.props.data.status === completeStatus ? 'line-through' : 'none' : 'none';
-        const value = this.props.data !== null ? this.props.data.name : 'Add task';
+        const placeholder = this.props.data !== null ? '' : 'Add task...';
+        const value = this.props.data !== null ? this.props.data.name : this.state.value;
 
         return (
             <div className={'taskItem'}>
                 <FontAwesomeIcon className={'taskCheckIcon'} icon={checkIcon} onClick={this.onCheckIconClick}></FontAwesomeIcon>
-                <textarea className={'taskDescription'} maxLength={'100'} rows={'2'} value={value} style={{textDecoration:descriptionDecoration}} onClick={this.onNameClick} onChange={this.onNameChange}></textarea>                
+                <textarea className={'taskDescription'} maxLength={'100'} rows={'2'} placeholder={placeholder} value={value} style={{textDecoration:descriptionDecoration}} onClick={this.onNameClick} onChange={this.onNameChange}></textarea>                
             </div>
                            
         )
