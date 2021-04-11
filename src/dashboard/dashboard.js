688c46eb67d246b9836891e4824bf4801cddd436
John Angel <j_angeldiaz@hotmail.com>
2021-04-10 19:23:31 -0500

    Dashboard component migrated to Hooks

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import './dashboard.css';
 import ProjectSummary from '../project/projectSummary';
@@ -12,70 +12,64 @@ import parseRevenue from '../utils/revenue';
 import TaskOverview from './taskOverview';
 import Search from '../search/search';
 
-class Dashboard extends Component{
+function Dashboard(props){
 
-    constructor(props){
-      super(props);
-      
-      this.state = {
-        projects: Object.keys(projects).map(item => projects[item]),
-        searchText:''
-      }
-    }  
+  const [projectsArray] = useState(Object.keys(projects).map(item => projects[item]));
+  const [searchText, setSearchText] = useState('');
 
-    parseFinishDate = (project) => {
+  const parseFinishDate = (project) => {
 
-      let dateArray = project.finishDate.split('-');      
-      let month = Number(dateArray[1]) - 1;
-      const date = new Date(dateArray[0], month, dateArray[2]);
+    let dateArray = project.finishDate.split('-');
+    let month = Number(dateArray[1]) - 1;
+    const date = new Date(dateArray[0], month, dateArray[2]);
 
-      dateArray = date.toDateString().split(' ');
+    dateArray = date.toDateString().split(' ');
 
-      return `${dateArray[2]} ${dateArray[1]} ${dateArray[3]}`;
-    }
+    return `${dateArray[2]} ${dateArray[1]} ${dateArray[3]}`;
+  }
 
-    onSearchEvent = (text) => {
-      this.setState({searchText:text})
-    }
-  
+  const onSearchEvent = (text) => {
+    setSearchText(text);
+  }
 
-    onClick = (projectId) => {
-      this.props.history.push({
-        pathname: `/project/${projectId}`,
-        state: { item: 'tasks' }
-      })
-    }
 
-    render(){
+  const onClick = (projectId) => {
+    props.history.push({
+      pathname: `/project/${projectId}`,
+      state: { item: 'tasks' }
+    })
+  }
 
-      return (
-        <div className='dashBoardContainer'>
-          <section className='dashboardSummary'>
-            <Search placeholder='Search projects...' onSearchEvent={this.onSearchEvent}></Search>
-            <ProjectSummary></ProjectSummary>
-            <RevenueSummary></RevenueSummary>
-          </section>
-          <section className='dashboardDetails'>
-            {
-              this.state.projects.map(project => {
-                return project.name.toLowerCase().includes(this.state.searchText.toLowerCase()) ?
-                (
-                  <DashboardItem key={project.id} title={project.name} onClickEvent={() => this.onClick(project.id)}>
-                    <p className='dashboardProjectStatus'>Status: {getTextFromStatus(project.status)}</p>
-                    <p className='dashboardProjectRevenue'>Revenue: {parseRevenue(project.revenue)}</p>
-                    <p className='dashboardProjectFinishDate'>Finish date: {this.parseFinishDate(project)}</p>
-                    <TaskOverview tasks={tasks[project.id]}></TaskOverview>
-                    <RevenueChart divId={'revenueChart' + project.id} projectId={project.id} revenue={revenue[project.id]}></RevenueChart>
-                    <ProgressChart tasksPercentage='20%' revenuePercentage='50%'></ProgressChart>
-                  </DashboardItem>
-                ):
-                null
-              })
-            }
-          </section>
-        </div>
-      )
-    }
+  return (
+
+
+    <div className='dashBoardContainer'>
+      <section className='dashboardSummary'>
+        <Search placeholder='Search projects...' onSearchEvent={onSearchEvent}></Search>
+        <ProjectSummary></ProjectSummary>
+        <RevenueSummary></RevenueSummary>
+      </section>
+      <section className='dashboardDetails'>
+        {
+          projectsArray.map(project => {
+            return project.name.toLowerCase().includes(searchText.toLowerCase()) ?
+              (
+                <DashboardItem key={project.id} title={project.name} onClickEvent={() => onClick(project.id)}>
+                  <p className='dashboardProjectStatus'>Status: {getTextFromStatus(project.status)}</p>
+                  <p className='dashboardProjectRevenue'>Revenue: {parseRevenue(project.revenue)}</p>
+                  <p className='dashboardProjectFinishDate'>Finish date: {parseFinishDate(project)}</p>
+                  <TaskOverview tasks={tasks[project.id]}></TaskOverview>
+                  <RevenueChart divId={'revenueChart' + project.id} projectId={project.id} revenue={revenue[project.id]}></RevenueChart>
+                  <ProgressChart tasksPercentage='20%' revenuePercentage='50%'></ProgressChart>
+                </DashboardItem>
+              ) :
+              null
+          })
+        }
+      </section>
+    </div>
+  )
+
 }
 
 export default withRouter(Dashboard);
